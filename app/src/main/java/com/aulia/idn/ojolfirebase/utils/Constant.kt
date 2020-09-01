0c3daf5be34ab01bb07a6d99d32a29036f498bcc
Dhiyaul Aulia <dhiyaul.aulia2003@gmail.com>
2020-09-01 17:36:08 +0700

    recommit

--- app/src/main/java/com/aulia/idn/ojolfirebase/utils/Constant.kt
+++ app/src/main/java/com/aulia/idn/ojolfirebase/utils/Constant.kt
@@ -7,6 +7,6 @@ object Constant {
     val baseUrlRoute = "https://maps.googleapis.com/maps/api/directions/"
     val baseUrlFcm = "https://fcm.googleapis.com"
     val key = "key"
-    val API_KEY = "AIzaSyCPy5d7fVtJN9U0cUM2Cj9i5_DNWG-9WtQ"
-//    val API_KEY=""
+//    val API_KEY = "AIzaSyCPy5d7fVtJN9U0cUM2Cj9i5_DNWG-9WtQ"
+    val API_KEY=""
 }
